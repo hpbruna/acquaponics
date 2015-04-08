8fecacdf5f1f3aea71e862596c434fef28c97d04
Bruna & Bruna <hp@brunabruna.nl>
2015-04-08 16:58:43 +0200

    Logging Opmerkingen Steven

--- mainprocess.py
+++ mainprocess.py
@@ -1,8 +1,9 @@
-import RPi.GPIO as GPIO ## Import GPIO Library
+import RPi.GPIO as GPIO #Import GPIO Library
 import time
 import datetime
+from pprint import pprint #data naar het scherm loggen.
 
-GPIO.setmode(GPIO.BCM) ## Gebruik Broadcom pin numbering
+GPIO.setmode(GPIO.BOARD)
 gpio_pump=1 #hier definieve pinnummers invoeren.
 gpio_drain=2
 tempPin = 3
@@ -33,40 +34,50 @@ def pumpcycle():
 	for i in range(0,cycle_count): ## Aantal keren pompen/afpompen
 
 		GPIO.output(gpio_pump, False) ## Pomp aan
+		pprint('Pomp 1 AAN')
 		time.sleep(pump_time*60) ## timer
 
 		GPIO.output(gpio_pump, True) ## Pomp uit
+		pprint('Pomp 1 UIT')
 		time.sleep(hold_time*60) ## Wachten
 
 		## Afpompen
 		GPIO.output(gpio_drain, False) ## 2e pomp aan
+		pprint('Pomp 2 AAN')
 		time.sleep(drain_time*60) ## timer
 
 		GPIO.output(gpio_drain, True) ## 2e pomp uit
+		pprint('Pomp 2 UIT')
 		time.sleep(pause_time*60) ## Wachten
 
 	GPIO.cleanup()
 
 def getTemp():
-temp = GPIO.input(tempPin)
-return temp
+	temp = GPIO.input(tempPin)
+	pprint('Temperatuur: ' + temp)
+	return temp
+
 def getLevel():
-niveau = GPIO.input(niveauPin)
-return niveau
+	niveau = GPIO.input(niveauPin)
+	pprint('Niveau: ' + niveau)
+	return niveau
+
 def getFlow():
-flow = GPIO.input(flowPin)
-return flow
+	flow = GPIO.input(flowPin)
+	pprint('Flow: ' + flow)
+	return flow
+
 def getPh():
-ph = GPIO.input(phPin)
-return ph
+	ph = GPIO.input(phPin)
+	pprint('PH: ' + ph)
+	return ph
 
 
 def main():
-
 	while True:
-		if getPh() < 6,5:
+		if getPh() < 6.5:
 			pumpcycle()
-		if getPh() > 7,5:
+		if getPh() > 7.5:
 			pumpcycle()
 		if getTemp() < 18:
 			pumpcycle()
@@ -77,7 +88,7 @@ def main():
 		if getLevel() > 110:
 			pumpcycle()
 
-
+		time.sleep(hold_time*60) ## Wachten tot een volgende meet en log ronde.
 
 	print('Dit proces zou door moeten lopen en nu gaat dat fout')
 
